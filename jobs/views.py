b75ebb3592e0e451ea168075655f14cddc5bcdab
linus145 <lakklplbros@gmail.com>
2026-05-08 16:49:51 +0530

    ui chnages

--- jobs/views.py
+++ jobs/views.py
@@ -30,7 +30,7 @@ class ResponseMixin:
         self, status_msg, message, data=None, status_code=status.HTTP_200_OK
     ):
         return Response(
-            {"status": status_msg, "message": message, "data": data or {}},
+            {"status": status_msg, "message": message, "data": data if data is not None else {}},
             status=status_code,
         )
 
--- startups/views.py
+++ startups/views.py
@@ -23,7 +23,7 @@ class ResponseMixin:
     """Standardized JSON response helper."""
     def build_response(self, status_msg, message, data=None, status_code=status.HTTP_200_OK):
         return Response(
-            {"status": status_msg, "message": message, "data": data or {}},
+            {"status": status_msg, "message": message, "data": data if data is not None else {}},
             status=status_code,
         )
 
